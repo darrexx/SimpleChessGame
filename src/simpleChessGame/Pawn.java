bed6a5297bb7ab1fc9c7b002cdfc6e3261038ec1
darrexx <timo.walter@udo.edu>
2017-07-04 22:29:50 +0200

    Implement Pawn ChessPiece

--- src/simpleChessGame/Pawn.java
+++ src/simpleChessGame/Pawn.java
@@ -1,11 +1,27 @@
 package simpleChessGame;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Pawn extends ChessPiece {
 
+    //Todo: Implement the en passant Move(https://en.wikipedia.org/wiki/En_passant)
     @Override
     public Collection<Field> canMoveTo() {
-        return null;
+        List<Field> fields = new ArrayList<>();
+        Board board = currentField.getBoard();
+        short currentX = currentField.getX();
+        short currentY = currentField.getY();
+
+        if(board.getFields()[currentX][currentY+1].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX][currentY+1]);
+        }
+
+        if(board.getFields()[currentX][currentY+2].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX][currentY+2]);
+        }
+
+        return fields;
     }
 }
