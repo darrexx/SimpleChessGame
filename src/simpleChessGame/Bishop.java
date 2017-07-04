450e6d303a07d15ede7dcf0ae6b138270bd000ba
darrexx <timo.walter@udo.edu>
2017-07-04 22:13:14 +0200

    Implement the Bishop ChessPiece

--- src/simpleChessGame/Bishop.java
+++ src/simpleChessGame/Bishop.java
@@ -1,12 +1,51 @@
 package simpleChessGame;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 public class Bishop extends ChessPiece {
 
     @Override
     public Collection<Field> canMoveTo() {
-        return null;
+        List<Field> fields = new ArrayList<>();
+        Board board = currentField.getBoard();
+        short currentX = currentField.getX();
+        short currentY = currentField.getY();
+
+        boolean canMoveUp = true;
+        boolean canMoveDown = true;
+        for (int i = 1; i + currentX < board.getFields().length; i++) {
+            if (currentY + i < board.getFields()[0].length && board.getFields()[currentX + i][currentY + i].canPlace(getColor(), getEnemyColor()) && canMoveUp) {
+                fields.add(board.getFields()[currentX + i][currentY + i]);
+            }
+            if(board.getFields()[currentX + i][currentY + i].isInUse()){
+                canMoveUp = false;
+            }
+            if (currentY - i >= 0 && board.getFields()[currentX + i][currentY - i].canPlace(getColor(), getEnemyColor()) && canMoveDown) {
+                fields.add(board.getFields()[currentX + i][currentY - i]);
+            }
+            if(board.getFields()[currentX + i][currentY - i].isInUse()){
+                canMoveDown = false;
+            }
+        }
+        canMoveUp = true;
+        canMoveDown = true;
+        for (int i = -1; currentX + i >= 0; i--) {
+            if (currentY + i < board.getFields()[0].length && board.getFields()[currentX + i][currentY + i].canPlace(getColor(), getEnemyColor()) && canMoveUp) {
+                fields.add(board.getFields()[currentX + i][currentY + i]);
+            }
+            if(board.getFields()[currentX + i][currentY + i].isInUse()){
+                canMoveUp = false;
+            }
+            if (currentY - i >= 0 && board.getFields()[currentX + i][currentY - i].canPlace(getColor(), getEnemyColor()) && canMoveDown) {
+                fields.add(board.getFields()[currentX + i][currentY - i]);
+            }
+            if(board.getFields()[currentX + i][currentY - i].isInUse()){
+                canMoveDown = false;
+            }
+        }
+        return fields;
     }
 }
