c7975eb607f9e9a469301406c092470072d722a0
darrexx <timo.walter@udo.edu>
2017-07-04 22:13:37 +0200

    Implement the King ChessPiece

--- src/simpleChessGame/King.java
+++ src/simpleChessGame/King.java
@@ -1,11 +1,49 @@
 package simpleChessGame;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class King extends ChessPiece {
 
     @Override
     public Collection<Field> canMoveTo() {
-        return null;
+        List<Field> fields = new ArrayList<>();
+        Board board = currentField.getBoard();
+        short currentX = currentField.getX();
+        short currentY = currentField.getY();
+
+        if(board.getFields()[currentX][currentY+1].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX][currentY+1]);
+        }
+
+        if(board.getFields()[currentX+1][currentY+1].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX+1][currentY+1]);
+        }
+
+        if(board.getFields()[currentX+1][currentY].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX+1][currentY]);
+        }
+
+        if(board.getFields()[currentX+1][currentY-1].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX+1][currentY-1]);
+        }
+
+        if(board.getFields()[currentX][currentY-1].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX][currentY-1]);
+        }
+
+        if(board.getFields()[currentX-1][currentY-1].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX-1][currentY-1]);
+        }
+
+        if(board.getFields()[currentX-1][currentY].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX-1][currentY]);
+        }
+
+        if(board.getFields()[currentX-1][currentY+1].canPlace(getColor(), getEnemyColor())){
+            fields.add(board.getFields()[currentX-1][currentY+1]);
+        }
+        return fields;
     }
 }
