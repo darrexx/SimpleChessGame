c3486fab5f945bb082dfbd871e269e306111e47d
darrexx <timo.walter@udo.edu>
2017-07-04 15:01:56 +0200

    Implement the Rook ChessPiece

--- src/simpleChessGame/Rook.java
+++ src/simpleChessGame/Rook.java
@@ -1,27 +1,38 @@
 package simpleChessGame;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 public class Rook extends ChessPiece {
 
     @Override
-    public boolean moveTo(Field field) {
-        return false;
-    }
-
-    @Override
     public Collection<Field> canMoveTo() {
-        return null;
-    }
-
-    @Override
-    public Collection<ChessPiece> isInDangerFrom() {
-        return null;
-    }
-
-    @Override
-    Collection<Field> canMoveToWithoutEndangered() {
-        return null;
+        List<Field> fields = new ArrayList<>();
+        Board board = currentField.getBoard();
+        short currentX = currentField.getX();
+        short currentY = currentField.getY();
+        for(int i = currentX+1; i<board.getFields().length; i++){
+            if(!board.getFields()[i][currentY].canPlace(getColor(), getEnemyColor())){
+                fields.add(board.getFields()[i][currentY]);
+            }
+        }
+        for(int i = currentX-1; i>=0; i--){
+            if(!board.getFields()[i][currentY].canPlace(getColor(), getEnemyColor())){
+                fields.add(board.getFields()[i][currentY]);
+            }
+        }
+        for(int i = currentY+1; i<board.getFields()[0].length; i++){
+            if(!board.getFields()[currentX][i].canPlace(getColor(), getEnemyColor())){
+                fields.add(board.getFields()[currentX][i]);
+            }
+        }
+        for(int i = currentY-1; i>=0; i--){
+            if(!board.getFields()[currentX][i].canPlace(getColor(), getEnemyColor())){
+                fields.add(board.getFields()[currentX][i]);
+            }
+        }
+        return fields;
     }
 }
