eb5aaa28ad64eab3b64686596812441b91dcdb85
darrexx <timo.walter@udo.edu>
2017-07-04 22:44:52 +0200

    Add Check if Pawn has Moved Add Constructor for Pawn

--- src/simpleChessGame/Pawn.java
+++ src/simpleChessGame/Pawn.java
@@ -6,6 +6,20 @@ import java.util.List;
 
 public class Pawn extends ChessPiece {
 
+    boolean hasMoved;
+
+    public Pawn(int id, String name, Field currentField, Color color){
+        this( id, name, currentField, true, color);
+    }
+
+    public Pawn(int id, String name, Field currentField, boolean isAlive, Color color){
+        this.id = id;
+        this.name = name;
+        this.currentField = currentField;
+        this.isAlive = isAlive;
+        this.color = color;
+    }
+
     //Todo: Implement the en passant Move(https://en.wikipedia.org/wiki/En_passant)
     @Override
     public Collection<Field> canMoveTo() {
@@ -18,10 +32,20 @@ public class Pawn extends ChessPiece {
             fields.add(board.getFields()[currentX][currentY+1]);
         }
 
-        if(board.getFields()[currentX][currentY+2].canPlace(getColor(), getEnemyColor())){
+        if(!hasMoved && board.getFields()[currentX][currentY+2].canPlace(getColor(), getEnemyColor())){
             fields.add(board.getFields()[currentX][currentY+2]);
         }
 
         return fields;
+    }
+
+    @Override
+    public boolean moveTo(Field field){
+        if(super.moveTo(field)){
+            hasMoved = true;
+            return true;
+        }
+        return false;
+
     }
 }
